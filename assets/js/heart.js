6df2d5113d3bd62dde08173daa55c39ccea8efa6
balajikasiraj07 <kasiraju.balaji@gmail.com>
2024-06-23 11:29:04 +0530

    js removal

--- assets/js/heart.js
+++
@@ -1,9 +0,0 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    var heartIcon = document.querySelector('.heart-icon');
-    heartIcon.addEventListener('click', function() {
-      this.classList.toggle('liked');
-    });
-  });
-  
-  
