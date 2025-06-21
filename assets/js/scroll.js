2a0cbb57cc6f0f2d4c59a33e6b1c491a419ef9f6
balajikasiraj07 <kasiraju.balaji@gmail.com>
2025-06-21 16:26:42 +0530

    orphaned scroll js

--- assets/js/scroll.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const ticker = document.getElementById('ticker');
-    const tickerContainer = document.querySelector('.ticker-container');
-
-    // Duplicate the ticker content for seamless scrolling
-    const clone = ticker.cloneNode(true);
-    tickerContainer.appendChild(clone);
-});
